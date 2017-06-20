e4973b91e1751f84308401865b8383cfec91ff7a
Mingming Fan <Mingming Fan>
2017-06-20 16:37:21 -0400

    solve the encoding issue

---
+++ ext/datetime.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#PYQT5 PyQt4’s QtGui module has been split into PyQt5’s QtGui, QtPrintSupport and QtWidgets modules
+
+from PyQt5 import QtWidgets
+#PYQT5 QDialog, QPushButton, QGridLayout, QComboBox
+
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import Qt
+
+from time import strftime
+
+class DateTime(QtWidgets.QDialog):
+    def __init__(self,parent=None):
+        QtWidgets.QDialog.__init__(self,parent)
+        self.parent = parent
+        self.formats = ["%A, %d. %B %Y %H:%M",
+                        "%A, %d. %B %Y",
+                        "%d. %B %Y %H:%M",
+                        "%d.%m.%Y %H:%M",
+                        "%d. %B %Y",
+                        "%d %m %Y",
+                        "%d.%m.%Y",
+                        "%x",
+                        "%X",
+                        "%H:%M"]
+        self.initUI()
+
+
+    def initUI(self):
+        self.box = QtWidgets.QComboBox(self)
+
+        for i in self.formats:
+            self.box.addItem(strftime(i))
+
+        insert = QtWidgets.QPushButton("Insert", self)
+        insert.clicked.connect(self.insert)
+
+        cancel = QtWidgets.QPushButton("cancel", self)
+        cancel.clicked.connect(self.close)
+
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.box,0,0,1,2)
+        layout.addWidget(insert,1,0)
+        layout.addWidget(cancel,1,1)
+
+        self.setGeometry(300,300,100,500)
+        self.setWindowTitle("Date adn Time")
+        self.setLayout(layout)
+
+    def insert(self):
+        cursor = self.parent.text.textCursor()
+        datetime = strftime(self.formats[self.box.currentIndex()])
+        cursor.insertText(datetime)
+        self.close()
